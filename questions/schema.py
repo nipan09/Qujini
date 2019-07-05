6a7f82eaeb14f99c57b7710ed5692481577ce295
Nishant Pandey <pnishant46@gmail.com>
2019-07-05 15:36:05 +0530

    docs: Mutation added

--- questions/schema.py
+++ questions/schema.py
@@ -1,14 +1,52 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import Type, Topic
+from .models import *
+
+class subject(graphene.InputObjectType):
+    topics= graphene.String(slug=graphene.String())
+    min_mark= graphene.Int()
+    max_mark= graphene.Int()
+
+class type(graphene.InputObjectType):
+    types= graphene.String(required= False, slug=graphene.String())
+    min_mark= graphene.Int(required= False)
+    max_mark= graphene.Int(required= False)
+
+class section(graphene.InputObjectType):
+    title= graphene.String()
+    marks= graphene.Int()
+    types= graphene.String(slug=graphene.String())
+    topics= graphene.String(slug= graphene.String())
+    
+
+class createquestion(graphene.Mutation):
+  class Arguments:
+     marks= graphene.Int(required= True)
+     difficulty= graphene.String() 
+     clubQuestions= graphene.Boolean()
+     optionalQuestions= graphene.Boolean()
+     savePreset= graphene.Boolean()
+     topics= graphene.List(subject)
+     types= graphene.List(type)
+     sections= graphene.List(section)
+  ok= graphene.Boolean()
+  #question= graphene.Field()
+   
+  def mutate(self, info):
+      question= quest(id=id)
+      ok=true
+      return createquestion(question=question, ok=ok)
+
+class MyMutations(graphene.ObjectType):
+    create_question= createquestion.Field()
 
 class TypeNode(DjangoObjectType):
-  class Meta:
-     model= Type
+    class Meta:
+       model= Type
 
 class TopicNode(DjangoObjectType):
-  class Meta:
-     model= Topic 
+    class Meta:
+       model= Topic 
 
 # Writing an app level query
 class Query(graphene.ObjectType):
@@ -20,4 +58,4 @@ class Query(graphene.ObjectType):
 
     
     def resolve_all_topic(self, info):
-       return Topic.objectsall()
+       return Topic.objectsall()          
--- qujini/schema.py
+++ qujini/schema.py
@@ -1,9 +1,27 @@
 import graphene
 import questions.schema 
 
-
 class Query(questions.schema.Query, graphene.ObjectType):
         # This class extends all abstract apps level Queries and graphene.ObjectType
-        pass
+        pass    
 
-schema = graphene.Schema(query=Query)
+class Mutation(questions.schema.MyMutations, graphene.ObjectType):  
+        # This class extends all abstract apps level Mutations and graphene.ObjectType
+     pass
+
+schema = graphene.Schema(query=Query,mutation= Mutation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#query=Query
+#questions.schema.Query,
